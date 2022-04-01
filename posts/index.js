347052b5dd081a74feb2ba1a568849dbd663d793
DhansAL <sgdhans@gmail.com>
2022-04-01 09:32:20 +0530

    event bus 404

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const { randomBytes } = require("crypto")
 const bodyParser = require("body-parser")
+const axios = require("axios")
+
 
 const cors = require("cors")
 const app = express()
@@ -15,12 +17,20 @@ const posts = {}
 app.get('/posts', (req, res) => {
     res.send(posts)
 })
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
     const id = randomBytes(4).toString("hex")
     const { title } = req.body;
     posts[id] = {
         id, title
     }
+
+    await axios.post('http://localhost:4005/events', {
+        type: "PostCreated",
+        data: {
+            id, title
+        }
+    })
+
     res.status(201).send(posts[id])
 
 })
